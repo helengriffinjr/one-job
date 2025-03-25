fc7a5cdab7a7758bff4820626b2f89f8c760e94a
Helen Griffin <helen@jovial.email>
2025-03-25 10:29:47 -0400

    Initial Commit

---
+++ one job/one_jobApp.swift
@@ -0,0 +1,17 @@
+//
+//  one_jobApp.swift
+//  one job
+//
+//  Created by Helen Griffin on 3/25/25.
+//
+
+import SwiftUI
+
+@main
+struct one_jobApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ one jobTests/one_jobTests.swift
@@ -0,0 +1,17 @@
+//
+//  one_jobTests.swift
+//  one jobTests
+//
+//  Created by Helen Griffin on 3/25/25.
+//
+
+import Testing
+@testable import one_job
+
+struct one_jobTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
